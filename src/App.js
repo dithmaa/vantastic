9df1777579e94743e2b6b27d3b68523c320fe70e
Baltukov Nomto <nomto_04@mail.ru>
2024-06-04 18:18:21 +0800

    fff

--- src/App.js
+++ src/App.js
@@ -4,7 +4,15 @@ import Cards from "./components/Cards/Cards";
 import CardPage from "./pages/card/card";
 import Form from "./pages/form/form";
 
+const tg = window.Telegram.WebApp;
+
 function App() {
+  useEffect(() => {
+    tg.ready();
+    setTimeout(() => {
+      tg.expand();
+    }, 200);
+  }, []);
   return (
     <div className="App">
       <Routes>
