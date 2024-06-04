465ed99873b311177c6bc32d033f1f9e9daafcb8
Baltukov Nomto <nomto_04@mail.ru>
2024-06-04 18:25:38 +0800

    fff

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ function App() {
   }, []);
   return (
     <div className="App">
-      lorem lorem
       <Routes>
         <Route path="/" element={<Cards />} />
         <Route path="/card/" element={<CardPage />} />
